c52db9a421dee06e45ceaecd291d560112432ab8
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2022-09-18 02:12:22 +0600

    organized git repo
    We will not push anything directly to the root directory. (specially not the js files)
    create branch dir/use existing >> implementation subject name >> code

---
+++ Schemas/doctor.models.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+
+const doctorSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    contact: {
+        type: String
+    },
+    // gender: {
+    //     type: String
+    // },
+    birthDate: {
+        type: Date
+    },
+    // specialities:[{
+    //     speciality:{
+    //         type: String
+    //     }
+    // }],
+    // chamberAddress: {
+    //     type: String
+    // },
+    // qualification: [{
+    //     degree: {
+    //         type: String
+    //     },
+    //     year: {
+    //         type: Date
+    //     }
+    // }],
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("doctor_schema",doctorSchema)
---
+++ Schemas/patient.models.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const patientSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  // email: {
+  //     type: String,
+  //     required: true,
+  //     unique: true,
+  // },
+  // contact: {
+  //     type: String
+  // },
+  // gender: {
+  //     type: String
+  // },
+  birthDate: {
+      type: Date
+  },
+  // weight: {
+  //     type: Number
+  // },
+  // bloodGroup: {
+  //     type: String
+  // },
+  password: {
+      type: String,
+      required: true
+  }
+});
+
+module.exports = mongoose.model("patient_schema",patientSchema)
