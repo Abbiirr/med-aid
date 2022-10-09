9e4d18102dd34bc2ac5326d530dcc520589c6cf4
Abir <abir.hossain2107@gmail.com>
2022-10-09 19:23:04 +0600

    Defined appointment model

--- Backend-Design/backend/models/appointment.models.js
+++ Backend-Design/backend/models/appointment.models.js
@@ -1,7 +1,26 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const appointmentSchema = mongoose.Schema({
-    
-})
+  patientID: {
+    type: String,
+    required: true,
+  },
+  doctor: {
+    type: String,
+    required: true,
+  },
+  center: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('appointment_schema', appointmentSchema)
+module.exports = mongoose.model("appointment_schema", appointmentSchema);
