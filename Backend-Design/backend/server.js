55aeb851e8f7fe3e5ddd6d23d3058729a1270fea
Abir <abir.hossain2107@gmail.com>
2022-10-10 12:44:44 +0600

    Added center to server

--- Backend-Design/backend/server.js
+++ Backend-Design/backend/server.js
@@ -17,6 +17,7 @@ app.use("/patient", require("./routes/PatientRoutes"));
 app.use("/disease", require("./routes/diseaseRoutes"));
 app.use("/search", require("./routes/searchRoutes"));
 app.use("/appointment", require("./routes/appointmentRoutes"));
+app.use("/center", require("./routes/centerRoutes"));
 
 app.listen(port, () => {
   console.log("listening on port " + port);
