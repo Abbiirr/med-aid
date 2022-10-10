8e5c9e277e2266cc43b973fc4dfdd80fcbc1785e
Abir <abir.hossain2107@gmail.com>
2022-10-10 12:03:56 +0600

    Created appointmentRoutes

---
+++ Backend-Design/backend/routes/appointmentRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
+const {
+  getAppointments,
+  setAppointment,
+  putAppointment,
+  deleteAppointment,
+} = require("../controllers/appointmentController");
+
+router.get("/", getAppointments);
+
+router.post(
+  "/",
+  // body("email").isEmail().normalizeEmail(),
+  // body("contact").isLength({ min: 11 }).isMobilePhone(),
+  // body("birthDate").isDate({ format: "YYYY-MM-DD" }),
+  // body("password").isLength({
+  //   minLength: 5,
+  //   minLowercase: 1,
+  //   minUppercase: 1,
+  //   minNumbers: 1,
+  // }),
+
+  setAppointment
+);
+
+router.put("/:id", putAppointment);
+
+router.delete("/:id", deleteAppointment);
+
+module.exports = router;
