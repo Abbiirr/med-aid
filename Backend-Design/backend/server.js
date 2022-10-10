d085790266359391c04b717937401d488a92f4c0
Abir <abir.hossain2107@gmail.com>
2022-10-10 12:09:06 +0600

    Added appointment to server

--- Backend-Design/backend/server.js
+++ Backend-Design/backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 3000;
 
@@ -12,11 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/doctor', require('./routes/doctorRoutes'));
-app.use('/patient', require('./routes/PatientRoutes'));
-app.use('/disease', require('./routes/diseaseRoutes'));
+app.use("/doctor", require("./routes/doctorRoutes"));
+app.use("/patient", require("./routes/PatientRoutes"));
+app.use("/disease", require("./routes/diseaseRoutes"));
 app.use("/search", require("./routes/searchRoutes"));
+app.use("/appointment", require("./routes/appointmentRoutes"));
 
 app.listen(port, () => {
-    console.log('listening on port ' + port);
-})
+  console.log("listening on port " + port);
+});
