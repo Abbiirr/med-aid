58995dc88936d58b8c0ef1326c7d2951a4cf474e
Abir <abir.hossain2107@gmail.com>
2022-10-14 19:12:19 +0600

    Built a basic react form which takes data

--- Frontend-Design/src/pages/appointment/appointment.js
+++ Frontend-Design/src/pages/appointment/appointment.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./appointment.css";
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+
   return (
     <div className="wrapper">
       <h1>How About Them Apples</h1>
-      <form>
+      {submitting && (
+        <div>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}>
+                <strong>{name}</strong>:{value.toString()}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Name</p>
-            <input name="name" />
+            <input name="name" onChange={handleChange} />
           </label>
         </fieldset>
         <button type="submit">Submit</button>
