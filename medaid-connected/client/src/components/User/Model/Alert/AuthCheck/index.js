9d4f73484374b3c03c20381fa0d4b81c6aa67c11
Abir Hossain <abir.hossain2107@gmail.com>
2022-10-19 12:34:30 +0600

    Login, registration button added to appointment error alert

--- medaid-connected/client/src/components/User/Model/Alert/AuthCheck/index.js
+++ medaid-connected/client/src/components/User/Model/Alert/AuthCheck/index.js
@@ -1,31 +1,56 @@
-import React from 'react'
-import './style.scss'
-import { Icon } from 'react-icons-kit'
-import { ic_close } from 'react-icons-kit/md'
-import {Images} from '../../../../../utils/Images'
+import React from "react";
+import "./style.scss";
+import { Icon } from "react-icons-kit";
+import { ic_close } from "react-icons-kit/md";
+import { Images } from "../../../../../utils/Images";
+import { NavLink, useHistory } from "react-router-dom";
+import {
+  ic_apps,
+  ic_people,
+  ic_info_outline,
+  ic_lock,
+} from "react-icons-kit/md";
 
 const Index = ({ message, hide }) => {
-    return (
-        <div className="alert-modal-backdrop">
-            <div className="flex-center flex-column">
-                <div className="card border-0 shadow">
-                    <div className="card-header border-0 text-right bg-white p-4">
-                        <button
-                            type="button"
-                            className="btn rounded-circle shadow-none"
-                            onClick={hide}
-                        >
-                            <Icon icon={ic_close} size={22} />
-                        </button>
-                    </div>
-                    <div className="card-body text-center px-4 pt-0 pb-5">
-                        <img src={Images.Unlock} className="img-fluid" alt="..." />
-                        <h6 className="mb-0">{message}</h6>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="alert-modal-backdrop">
+      <div className="flex-center flex-column">
+        <div className="card border-0 shadow">
+          <div className="card-header border-0 text-right bg-white p-4">
+            <button
+              type="button"
+              className="btn rounded-circle shadow-none"
+              onClick={hide}
+            >
+              <Icon icon={ic_close} size={22} />
+            </button>
+          </div>
+          <div className="card-body text-center px-4 pt-0 pb-5">
+            <img src={Images.Unlock} className="img-fluid" alt="..." />
+            <h6 className="mb-0">{message}</h6>
+            <NavLink
+              exact
+              activeClassName="is-Active"
+              className="btn btn-block shadow-none"
+              to="/login"
+            >
+              <Icon icon={ic_apps} size={20} />
+              <span>Login</span>
+            </NavLink>
+            <NavLink
+              exact
+              activeClassName="is-Active"
+              className="btn btn-block shadow-none"
+              to="/register"
+            >
+              <Icon icon={ic_apps} size={20} />
+              <span>Register</span>
+            </NavLink>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Index;
