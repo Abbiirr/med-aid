988f972a2f52afe1c383966f53e637caf9a75b18
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2022-10-22 23:48:12 +0600

    symptom based search working
    logical error inside find method of search controller

--- medaid-connected/server/api/routes/patient.js
+++ medaid-connected/server/api/routes/patient.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const Authenticate = require('../middleware/Permission')
 const ProfileController = require('../controllers/Patient/ProfileController')
 const AppointmentController = require('../controllers/Patient/AppointmentController')
+const searchDoctorsController = require('../controllers/Patient/SearchDoctor')
 
 router.get('/me', Authenticate.isPatient, ProfileController.Me)
 router.post('/profile/:id/update/photo', Authenticate.isPatient, ProfileController.updatePhoto)
 router.post('/profile/:id/update/bio', Authenticate.isPatient, ProfileController.updateBio)
+
+router.get('/findDoctors', searchDoctorsController.findNearestDoctors);
+router.get('/reload', searchDoctorsController.reloadSearch);
 
 router.get('/appointment/request/:id/index', Authenticate.isPatient, AppointmentController.GetAppointmentRequests)
 router.post('/appointment/request', Authenticate.isPatient, AppointmentController.SetAppointmentRequest)
