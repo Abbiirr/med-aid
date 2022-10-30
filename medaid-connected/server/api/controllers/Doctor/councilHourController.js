922264f25e9d7cd980078e28ff157d5b1006fd53
Abir <abir.hossain2107@gmail.com>
2022-10-30 11:31:52 +0600

    Server side request for councilHours

---
+++ medaid-connected/server/api/controllers/Doctor/councilHourController.js
@@ -0,0 +1,29 @@
+const Council = require("../../../models/Council");
+const CheckId = require("../../middleware/CheckId");
+
+// Appointment Requests
+const getCouncilHours = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log(req.params);
+    // await CheckId(id);
+    // var o_id = new ObjectId(id);
+    const results = await Council.find({ _id: id });
+    if (!results.length)
+      return res.status(404).json({
+        status: false,
+        message: "Request not found",
+      });
+
+    res.status(200).json({
+      status: true,
+      requests: results,
+    });
+  } catch (error) {
+    if (error) next(error);
+  }
+};
+
+module.exports = {
+  getCouncilHours,
+};
--- medaid-connected/server/api/routes/doctor.js
+++ medaid-connected/server/api/routes/doctor.js
@@ -1,19 +1,37 @@
-const express = require('express')
-const router = express.Router()
-const AuthController = require('../middleware/Permission')
-const ProfileController = require('../controllers/Doctor/ProfileController')
-const AppointmentController = require('../controllers/Doctor/AppointmentController')
-const DoctorController = require('../controllers/Doctor/DoctorController')
+const express = require("express");
+const router = express.Router();
+const AuthController = require("../middleware/Permission");
+const ProfileController = require("../controllers/Doctor/ProfileController");
+const AppointmentController = require("../controllers/Doctor/AppointmentController");
+const DoctorController = require("../controllers/Doctor/DoctorController");
+const CouncilHourController = require("../controllers/Doctor/councilHourController");
 
-router.get('/me', ProfileController.Me)
-router.post('/profile/:id/update', AuthController.isDoctor, ProfileController.updateProfile)
+router.get("/me", ProfileController.Me);
+router.post(
+  "/profile/:id/update",
+  AuthController.isDoctor,
+  ProfileController.updateProfile
+);
 
-router.get('/getDoctors', DoctorController.getDoctors);
-router.get('/getDoctorsResult', DoctorController.getDoctorsResult);
-router.get('/reload', DoctorController.reloadSearch);
+router.get("/getDoctors", DoctorController.getDoctors);
+router.get("/getDoctorsResult", DoctorController.getDoctorsResult);
+router.get("/reload", DoctorController.reloadSearch);
 
-router.get('/appointment/:id/requests', AuthController.isDoctor, AppointmentController.AppointmentRequests)
-router.get('/appointment/:id/approved', AuthController.isDoctor, AppointmentController.ApprovedAppointments)
-router.put('/appointment/approve', AuthController.isDoctor, AppointmentController.ApproveAppointment)
+router.get(
+  "/appointment/:id/requests",
+  AuthController.isDoctor,
+  AppointmentController.AppointmentRequests
+);
+router.get(
+  "/appointment/:id/approved",
+  AuthController.isDoctor,
+  AppointmentController.ApprovedAppointments
+);
+router.put(
+  "/appointment/approve",
+  AuthController.isDoctor,
+  AppointmentController.ApproveAppointment
+);
+router.get("/councils/:id", CouncilHourController.getCouncilHours);
 
-module.exports = router
+module.exports = router;
