37b700fee53503e1fff899081d3a09c2dfd0b19b
Abir <abir.hossain2107@gmail.com>
2022-11-09 19:26:08 +0600

    Successfully adding councilHours to councils array

--- medaid-connected/server/api/controllers/Doctor/DoctorController.js
+++ medaid-connected/server/api/controllers/Doctor/DoctorController.js
@@ -1,6 +1,7 @@
 const Doctor = require("../../../models/Doctor");
 const disease_schema = require("../../../models/Disease");
 const Council = require("../../../models/Council");
+const CheckId = require("../../middleware/CheckId");
 
 let diseaseArray = new Set();
 let specialtyArray = new Set();
@@ -11,27 +12,82 @@ const getDoctors = async (req, res) => {
   res.json(doctors);
 };
 
-const updateCouncils = async (req, res) => {
-  const newCouncil = new Council({
-    doctor: doctor._id,
-    schedule: { day: day, startTime: startTime, endTime: endTime },
-  });
+const Me = async (req, res, next) => {
+  try {
+    // Split token
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = jwt.verify(token, "SECRET");
 
-  let council = await newCouncil.save();
-
-  // set council into doctor
-  const updateDoctor = await doctor
-    .update(
+    // Find account using account id and role
+    let account = await Doctor.findOne(
       {
-        $push: {
-          councilHour: [council._id],
-        },
+        $and: [{ _id: decode.id }, { role: decode.role }],
       },
-      { new: true }
+      { access_token: 0, password: 0 }
     )
-    .exec();
+      .populate("councilHour")
+      .exec();
 
-  updateDoctor();
+    if (!account) {
+      return res.status(404).json({
+        status: false,
+        message: "Invalid token",
+      });
+    }
+
+    for (const property in account) {
+      if (property === "image")
+        account[property] =
+          hostURL(req) + "uploads/doctor/profiles/" + account[property];
+    }
+
+    return res.status(200).json({
+      status: true,
+      doctor: account,
+    });
+  } catch (error) {
+    if (error) next(error);
+  }
+};
+
+const updateCouncils = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { day, startTime, endTime } = req.body;
+
+    await CheckId(id);
+
+    // Find Profile
+    const doctor = await Doctor.findById({ _id: id }).exec();
+    if (!doctor) {
+      return res.status(404).json({
+        status: false,
+        message: "Doctor not found",
+      });
+    }
+    const newCouncil = new Council({
+      doctor: doctor._id,
+      schedule: { day: day, startTime: startTime, endTime: endTime },
+    });
+
+    let council = await newCouncil.save();
+
+    // set council into doctor
+    const updateDoctor = await doctor
+      .updateOne(
+        {
+          $push: {
+            councilHour: [council._id],
+          },
+        },
+        { new: true }
+      )
+      .exec();
+
+    // updateDoctor();
+  } catch (error) {
+    if (error) next(error);
+  }
 };
 const getDoctorsResult = async (req, res) => {
   // // getting the symptoms
