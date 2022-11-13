432fc34344cbbe6cbca803d310c5d5e63b935861
Abir <abir.hossain2107@gmail.com>
2022-11-13 18:41:14 +0600

    Configured medicine controller

--- medaid-connected/server/api/controllers/Medicine/medicineController.js
+++ medaid-connected/server/api/controllers/Medicine/medicineController.js
@@ -1,48 +1,74 @@
-const medicine_schema = require('../../../models/Medicine');
+const Medicine = require("../../../models/Medicine");
 
 //
 
 //--------- main med-aid get set delete update for disease------------------------
 const getMedicines = async (req, res) => {
-    const medicines = await medicine_schema.find()   
-    
-    res.json(medicines);
+  const medicines = await Medicine.find();
+
+  res.json(medicines);
 };
 
 const setMedicine = async (req, res) => {
+  try {
+    const { name, price } = req.body;
 
-    const medicine = await medicine_schema.create({
-        //------------------------
-    })
+    const check = await Medicine.findOne({ name: name }).exec();
+
+    if (check)
+      return res.status(208).json({
+        status: false,
+        message: "This medicine already exists.",
+      });
+
+    const newMedicine = await Medicine.create({
+      name: name,
+      price: price,
+    });
+    const createMedicine = await newMedicine.save();
 
     console.log(req.body);
-    res.json(medicine);
+    res.json(newMedicine);
+    if (createMedicine)
+      return res.status(201).json({
+        status: true,
+        message: "New Medicine have been added.",
+      });
+  } catch (error) {
+    if (error) {
+      console.log(error);
+      //   return res.status(300).json({
+      //     status: false,
+      //     message: error,
+      //   });
+      //   next(error);
+    }
+  }
 };
 
 const putMedicine = async (req, res) => {
-    const medicine = await medicine_schema.findByIdAndUpdate(
-      req.params.id,
-      {
-       //----------------
-      },
-      { new: true }
-    );
+  const medicine = await Medicine.findByIdAndUpdate(
+    req.params.id,
+    {
+      //----------------
+    },
+    { new: true }
+  );
 
-    res.json(medicine);
+  res.json(medicine);
 };
 
 const deleteMedicine = async (req, res) => {
-    const medicine = await medicine_schema.findByIdAndDelete(req.params.id)
-    center.delete();
-    res.json(medicine);
+  const medicine = await Medicine.findByIdAndDelete(req.params.id);
+  center.delete();
+  res.json(medicine);
 };
 
 //
 
-
 module.exports = {
-    getMedicines,
-    setMedicine,
-    putMedicine,
-    deleteMedicine,
+  getMedicines,
+  setMedicine,
+  putMedicine,
+  deleteMedicine,
 };
