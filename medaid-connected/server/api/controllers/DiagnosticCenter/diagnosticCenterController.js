ca32ba28c583aeb77c5ca44a6d136fb7d68fcd91
Abir <abir.hossain2107@gmail.com>
2022-11-13 19:07:47 +0600

    Configured diagonostic center controller

--- medaid-connected/server/api/controllers/DiagnosticCenter/diagnosticCenterController.js
+++ medaid-connected/server/api/controllers/DiagnosticCenter/diagnosticCenterController.js
@@ -1,58 +1,81 @@
-const center_schema = require('../../../models/DiagnosticCenter');
+const Center = require("../../../models/DiagnosticCenter");
 
 //
 
 //--------- main med-aid get set delete update for disease------------------------
-const getCenter= async (req, res) => {
-    const centers = await center_schema.find()   
-    
-    res.json(centers);
+const getCenter = async (req, res) => {
+  const centers = await Center.find();
+
+  res.json(centers);
 };
 
 const setCenter = async (req, res) => {
+  try {
+    const { name, location } = req.body;
 
-    const center = await center_schema.create({
-        name: req.body.name,
-        // tests: [{
-        //     test_name: req.body.test_name, 
-        //     test_cost: req.body.test_cost
-        // }],
-        location: req.body.location,
-    })
+    const check = await Center.findOne({ name: name }).exec();
+
+    if (check)
+      return res.status(208).json({
+        status: false,
+        message: "This Center already exists.",
+      });
+
+    const newCenter = await Center.create({
+      name: name,
+      location: location,
+    });
+    const createCenter = await newCenter.save();
 
     console.log(req.body);
-    res.json(center);
+    res.json(newCenter);
+    if (createCenter)
+      return res.status(201).json({
+        status: true,
+        message: "New Center have been added.",
+      });
+  } catch (error) {
+    if (error) {
+      console.log(error);
+      //   return res.status(300).json({
+      //     status: false,
+      //     message: error,
+      //   });
+      //   next(error);
+    }
+  }
 };
 
 const putCenter = async (req, res) => {
-    const center = await center_schema.findByIdAndUpdate(
-      req.params.id,
-      {
-        name: req.body.name,
-        tests: [{
-            test_name: req.body.test_name, 
-            test_cost: req.body.test_cost
-        }],
-        location: req.body.location,
-      },
-      { new: true }
-    );
+  const center = await Center.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      tests: [
+        {
+          test_name: req.body.test_name,
+          test_cost: req.body.test_cost,
+        },
+      ],
+      location: req.body.location,
+    },
+    { new: true }
+  );
 
-    res.json(disease);
+  res.json(disease);
 };
 
 const deleteCenter = async (req, res) => {
-    const center = await center_schema.findByIdAndDelete(req.params.id)
-    center.delete();
-    res.json(center);
+  const center = await Center.findByIdAndDelete(req.params.id);
+  center.delete();
+  res.json(center);
 };
 
 //
 
-
 module.exports = {
-    getCenter,
-    setCenter,
-    putCenter,
-    deleteCenter,
+  getCenter,
+  setCenter,
+  putCenter,
+  deleteCenter,
 };
