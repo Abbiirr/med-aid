556b8707e94c7fcb8d565be1bf52326d78f95745
Abir <abir.hossain2107@gmail.com>
2022-12-27 18:36:47 +0600

    Revert "Working on maps"
    This reverts commit 5316823bbec91fe2b060f9334718e1ff91013841.

--- medaid-connected/client/src/pages/DiagnosticCenter/diagnosticPage.js
+++ medaid-connected/client/src/pages/DiagnosticCenter/diagnosticPage.js
@@ -1,52 +1,165 @@
-import React, { useState, useEffect, useCallback, Component } from "react";
-import { Map, Wrapper, Marker } from "google-map-react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { apiURL } from "../../utils/apiURL";
+import queryString from "query-string";
+import { useLocation } from "react-router";
+import Select from 'react-select'
 
-export class MapContainer extends Component {
-  constructor(props) {
-    super(props);
+import "./style.scss";
 
-    this.state = {
-      stores: [
-        { lat: 47.49855629475769, lng: -122.14184416996333 },
-        { latitude: 47.359423, longitude: -122.021071 },
-        { latitude: 47.2052192687988, longitude: -121.988426208496 },
-        { latitude: 47.6307081, longitude: -122.1434325 },
-        { latitude: 47.3084488, longitude: -122.2140121 },
-        { latitude: 47.5524695, longitude: -122.0425407 },
-      ],
+import NavbarComponent from "../../components/User/Navbar/index";
+//import SearchComponent from "../../components/User/SearchForMedicine/index";
+import DiagnosticCenterListComponent from "../../components/diagnosticCenterList/diagnosticComponent";
+import FooterComponent from "../../components/User/Footer/index";
+
+const Index = () => {
+  //use a variable to store the search query
+  const [centers, setCenters] = useState([]);
+  const [tests, setTests] = useState([]);
+  const [allCenters, setAllCenters] = useState([]);
+  const [q, setQ] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [message, setMessage] = useState("");
+  const [searchTest, setSearchTest] = useState("");
+  const [searched, setSearched] = useState(false);
+    
+
+  const location = useLocation();
+  //const value = queryString.parse(location.search);
+
+  useEffect(() => {
+    const searchCenters = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/api/v1/diagnosticCenter`
+        );
+        setCenters(response.data);
+        setAllCenters(response.data);
+        //--------------getting the tests----------------
+        // let tempTests = new Set();
+        // allCenters.forEach((center) => {
+        //   center.tests.forEach((test) => {
+        //     tempTests.add(test.test_name);
+        //   });
+        // });
+        // setTests(tempTests);
+        // console.log("tests : ", tests);
+        //-----------------end of getting the tests----------------
+      } catch (error) {
+        if (error) console.log("error");
+      }
     };
-  }
+    searchCenters();
+  }, []);
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return (
-        <Marker
-          key={index}
-          id={index}
-          position={{
-            lat: store.latitude,
-            lng: store.longitude,
-          }}
-          onClick={() => console.log("You clicked me!")}
-        />
+  //console.log(q);
+
+  const handleChange = e => {
+    setSearchInput(e.target.value)
+ };
+
+ const submitSearch = async (e) =>{
+    e.preventDefault();
+    //dispatch(fetchSearch(searchInput));
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/api/v1/diagnosticCenter/findCenter?testName=${searchInput}`
       );
-    });
-  };
+      console.log("Test based center : ", response.data);
 
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={8}
-        style={mapStyles}
-        initialCenter={{ lat: 47.444, lng: -122.176 }}
-      >
-        {this.displayMarkers()}
-      </Map>
-    );
+      //---sorting based on searched test price
+      let temp =response.data
+      temp.sort((a, b) => {
+        let testA = a.tests.find((test) => test.test_name === searchInput);
+        let testB = b.tests.find((test) => test.test_name === searchInput);
+        return testA.test_cost - testB.test_cost;
+      });
+      //---end of sorting based on searched test price
+      setCenters(temp);
+      setSearchTest(searchInput);
+      setSearched(true);
+      console.log("test-price based center : ", temp)
+      //setCenters(response.data);
+      setMessage("Search results for " + searchInput);
+    } catch (error) {
+      if (error) console.log("error");
+    }
+    console.log(searchInput)
+    setSearchInput("");
   }
-}
 
-export default Wrapper({
-  apiKey: "TOKEN HERE",
-})(MapContainer);
+  //let centerOptions = []
+  let centerOptions2 = []
+
+  // allCenters.forEach( function (item){
+  //     centerOptions.push({
+  //         label: item.name,
+  //         value: item.name
+  //     })
+  // })
+
+  allCenters.forEach( function (item){
+    item.tests.forEach( function (test){
+      centerOptions2.push({
+        label: test.test_name,
+        value: test.test_name,
+      });
+    })
+  })
+  //create a set to remove duplicate values from array of 2 properties
+  const centerOptionsSet = new Set(centerOptions2.map(JSON.stringify));
+  const centerOptions3 = Array.from(centerOptionsSet).map(JSON.parse);
+
+
+  console.log("options : ",centerOptions2)
+
+
+
+  return (
+    <div>
+      <NavbarComponent />
+      <section className="garamond">
+          <div className="navy georgia ma0 grow">
+              <h2 className="f2">Search your tests</h2>
+          </div>
+          <div className="pa2">
+              <Select
+                  onChange={(item) => setSearchInput(item.value)}
+                  maxMenuHeight={175}
+                  classNamePrefix="custom-select"
+                  options={centerOptions3}
+                  //options={symptoms}
+                  //isMulti
+                  //isClearable={true}
+                  isSearchable={true}
+                  placeholder="Your tests??"
+                  // have to make this field required to make the search work
+              />
+              <button onClick={submitSearch} type="submit" className="btn">Search</button>
+          </div>
+
+      </section> 
+      <div className="search-result-index">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 py-4"></div>
+            <div className="col-12 py-4 py-lg-5 text-center">
+              <h3 className="font-weight-bold mb-0">
+                Found {centers ? centers.length : null} centers.
+              </h3>
+              <h5>
+                {message ? message : null}
+              </h5>
+            </div>
+          </div>
+        </div>
+
+        {/* Results */}
+        <DiagnosticCenterListComponent centers={centers} loading={false} searched={searched} testName={searchTest}/>
+      </div>
+      <FooterComponent />
+    </div>
+  );
+};
+
+export default Index;
