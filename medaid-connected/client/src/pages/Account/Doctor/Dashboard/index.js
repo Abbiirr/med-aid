6e3739757120768e6738398e5002fe52a944c995
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2022-12-28 14:52:53 +0600

    Frontend - Active PA's

--- medaid-connected/client/src/pages/Account/Doctor/Dashboard/index.js
+++ medaid-connected/client/src/pages/Account/Doctor/Dashboard/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import "./style.scss";
 import { Bar } from "react-chartjs-2";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Badge from '@mui/material/Badge';
 
 const Index = () => {
   const [year] = useState(new Date().getFullYear());
@@ -43,79 +49,103 @@ const Index = () => {
       },
     ],
   });
+  const [invisible, setInvisible] = React.useState(false);
+
+  const handleBadgeVisibility = () => {
+    setInvisible(!invisible);
+  };
   return (
-    <div className="dashboard">
-      <div className="container-fluid pl-lg-0 py-3 py-lg-0">
-        <div className="row">
-          <div className="col-12 col-lg-6 mb-3">
-            <div className="card border-0">
-              <div className="card-body">
-                {/* <Bar
-                  data={data}
-                  width={800}
-                  height={400}
-                  options={{
-                    maintainAspectRatio: false,
-                    title: {
-                      display: true,
-                      text: "Patient in " + year,
-                      fontSize: 20,
-                    },
-                    legend: {
-                      display: true,
-                      position: "bottom",
-                    },
-                  }}
-                /> */}
+    <div>
+      <div className="dashboard">
+        <div className="container-fluid pl-lg-0 py-3 py-lg-0">
+          <div className="row">
+            <div className="col-12 col-lg-6 mb-3">
+              <div className="card border-0">
+                <div className="card-body">
+                  <Bar
+                    data={data}
+                    width={800}
+                    height={400}
+                    options={{
+                      maintainAspectRatio: false,
+                      title: {
+                        display: true,
+                        text: "Patient in " + year,
+                        fontSize: 20,
+                      },
+                      legend: {
+                        display: true,
+                        position: "bottom",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-lg-6">
+              <div className="row">
+                <div className="col-6 mb-3 pl-lg-0 pr-0">
+                  <div className="card border-0" style={{ height: 150 }}>
+                    <div className="card-body">
+                      <div className="flex-center flex-column text-center">
+                        <h5 className="mb-0">120</h5>
+                        <h6 className="mb-0">Appointments</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-6 mb-3">
+                  <div className="card border-0" style={{ height: 150 }}>
+                    <div className="card-body">
+                      <div className="flex-center flex-column text-center">
+                        <h5 className="mb-0">120</h5>
+                        <h6 className="mb-0">Today Appointments</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6 mb-3 pl-lg-0 pr-0">
+                  <div className="card border-0" style={{ height: 150 }}>
+                    <div className="flex-center flex-column text-center">
+                      <h5 className="mb-0">120</h5>
+                      <h6 className="mb-0">New Request</h6>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-6 mb-3">
+                  <div className="card border-0" style={{ height: 150 }}>
+                    <div className="card-body">
+                      <div className="flex-center flex-column text-center">
+                        <h5 className="mb-0">120</h5>
+                        <h6 className="mb-0">Total Patient</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          {/* <div className="col-12 col-lg-6">
-            <div className="row">
-              <div className="col-6 mb-3 pl-lg-0 pr-0">
-                <div className="card border-0" style={{ height: 150 }}>
-                  <div className="card-body">
-                    <div className="flex-center flex-column text-center">
-                      <h5 className="mb-0">120</h5>
-                      <h6 className="mb-0">Appointments</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-6 mb-3">
-                <div className="card border-0" style={{ height: 150 }}>
-                  <div className="card-body">
-                    <div className="flex-center flex-column text-center">
-                      <h5 className="mb-0">120</h5>
-                      <h6 className="mb-0">Today Appointments</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-6 mb-3 pl-lg-0 pr-0">
-                <div className="card border-0" style={{ height: 150 }}>
-                  <div className="flex-center flex-column text-center">
-                    <h5 className="mb-0">120</h5>
-                    <h6 className="mb-0">New Request</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="col-6 mb-3">
-                <div className="card border-0" style={{ height: 150 }}>
-                  <div className="card-body">
-                    <div className="flex-center flex-column text-center">
-                      <h5 className="mb-0">120</h5>
-                      <h6 className="mb-0">Total Patient</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Active PA's
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Badge color="primary" variant="dot" invisible={invisible}>
+              <Avatar alt="Sabil Azmayen" src="/static/images/avatar/1.jpg" />
+            </Badge>
+            <Badge color="primary" variant="dot" invisible={invisible}>
+              <Avatar alt="Abir Hossain" src="/static/images/avatar/1.jpg" />
+            </Badge>
+            <Avatar alt="Dipu" src="/static/images/avatar/1.jpg" />
+          </Stack>
+        </CardContent>
+      </Card>
     </div>
   );
 };
